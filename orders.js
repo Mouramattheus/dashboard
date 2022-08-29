af2a31839e17b04195af3d93c96a20b0e747fb7b
Matheus Augusto Moura de Lima <61365431+Mouramattheus@users.noreply.github.com>
2022-08-29 09:12:25 -0300

    finalizado

---
+++ orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        productName: 'Notbook Asus',
+        produtNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Max-Steel',
+        produtNumber: '56331',
+        paymentStatus: 'Due',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Barbie',
+        produtNumber: '25631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Iphone 22r',
+        produtNumber: '98732',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Alexa 298r',
+        produtNumber: '85631',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Sony PS5',
+        produtNumber: '66621',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+]
